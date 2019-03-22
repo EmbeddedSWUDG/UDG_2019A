e78044ae4a83e3a1788b0d469515c636e36274e4
Leone111 <48542291+Leone111@users.noreply.github.com>
2019-03-21 18:58:42 -0600

    example 3 spaces

--- FSM.c
+++ FSM.c
@@ -4,6 +4,11 @@
 /*
  * Copyright 2016-2018 NXP
  * All rights reserved.
+ 
+ 
+ 
+ 
+ 
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
